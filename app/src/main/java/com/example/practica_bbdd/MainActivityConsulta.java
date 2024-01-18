e6d0ad5a81e65717b6ccfc0263824c2fc6b0b128
maikel50 <palango4887@gmail.com>
2024-01-18 13:01:22 +0100

    creacion pagina consulta

---
+++ app/src/main/java/com/example/practica_bbdd/MainActivityConsulta.java
@@ -0,0 +1,15 @@
+package com.example.practica_bbdd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivityConsulta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_consulta);
+    }
+
+}
